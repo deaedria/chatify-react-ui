c39fbedfdf01278ab20585b2909e8e32ba561aaa
deaedria <dea.edria@gmail.com>
2021-09-01 22:58:11 +0700

    edit with useLocation (react-router-dom)

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import moment, { updateLocale } from 'moment'
 import { useDispatch } from "react-redux"
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import { DelChats } from "../Redux/Actions/chats"
 
 const Chat = (props) => {
@@ -19,6 +19,7 @@ const Chat = (props) => {
 
     const dispatch = useDispatch()
     let history = useHistory();
+    let location = useLocation();
 
     return (
         <ul className="list">
@@ -81,7 +82,7 @@ const Chat = (props) => {
                         <div className="btn-icon">
                             <img src="/svg/bookmark-white-icon.svg" alt="save chat" />
                             <img src="/svg/check-white-icon.svg" alt="read chat" />
-                            <img src="/svg/delete-icon.svg" alt="delete chat" onClick={() => { dispatch(DelChats(sc_id)); window.location.reload();  }} />
+                            <img src="/svg/delete-icon.svg" alt="delete chat" onClick={() => { dispatch(DelChats(sc_id)); history.push(location.pathname)  }} />
                         </div>
                     </div>
                 </li>
--- src/Pages/Message/index.js
+++ src/Pages/Message/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory, useParams } from "react-router-dom";
+import { useHistory, useParams, useLocation } from "react-router-dom";
 import { withRouter } from "react-router";
 import { useDispatch, useSelector } from "react-redux"
 import { FetchChats } from "../../Redux/Actions/chats"
@@ -22,6 +22,7 @@ const Message = () => {
     // console.log(data)
 
     let history = useHistory();
+    let location = useLocation();
     const dispatch = useDispatch()
 
     const menuToggle = () => {
@@ -63,7 +64,7 @@ const Message = () => {
             dispatch(AddMessages(userToken, dataToken, contact, myData))
             dispatch(FetchMessages(userToken, dataToken, contact))
             dispatch(FetchChats(userToken, dataToken))
-            window.location.reload(); 
+            history.push(location.pathname)
             // document.querySelector(".box-list").scrollTop = document.querySelector(".box-list").scrollHeight
         }
     }
@@ -84,7 +85,7 @@ const Message = () => {
                 dispatch(FetchMessages(userToken, dataToken, contact))
                 dispatch(FetchChats(userToken, dataToken))
                 document.getElementById("input").value = "";
-                window.location.reload(); 
+                history.push(location.pathname)
             } else {
                 // alert("your massage must be filled")
             }
